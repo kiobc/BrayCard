72110eb26f3c8e55a8175e49b11ca0bb52fa9a5c
kiobc <kiobc1992@gmail.com>
2023-03-17 21:40:07 -0500

    Agregado los diferentes tipos de cartas

---
+++ js/CartaTipo.js
@@ -0,0 +1,59 @@
+export let CartaTipo = [
+    {
+      name: 'Posion menor',
+      value: 3,
+      image: 'healingpotion',
+      type: 'heal'
+    },
+    {
+      name: 'Posion Maxima',
+      value: 6,
+      image: 'healingpotion',
+      type: 'heal'
+    },
+    {
+      name: 'Escudo',
+      value: 5,
+      image: 'shield',
+      type: 'armor'
+    },
+    {
+      name: 'Kobold',
+      value: 6,
+      image: 'kobold',
+      type: 'attack'
+    },
+    {
+      name: 'Troll',
+      value: 8,
+      image: 'troll',
+      type: 'attack'
+    },
+    {
+      name: 'Ogro',
+      value: 10,
+      image: 'ogre',
+      type: 'attack'
+    },
+    {
+      name: 'Caballero de la muerte',
+      value: 12,
+      image: 'deathknight',
+      type: 'attack'
+    },
+    {
+      name: 'Dragon de Fuego',
+      value: 16,
+      image: 'firedrake',
+      type: 'attack'
+    },
+    {
+      name: 'Dragón Dorado',
+      value: 20,
+      image: 'goldendragon',
+      type: 'attack'
+    }
+  ];
+  
+  export default CartaTipo;
+  
